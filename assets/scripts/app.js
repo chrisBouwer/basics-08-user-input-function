3e0d4bf2b10e0bd0707831a89cf28029ebb551ba
chrisBouwer <chrisbouwer01@gmail.com>
2021-09-29 10:34:22 +0200

    Added functions for each calc button & display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserNumberInput(){
+function getUserNumberInput() {
   return parseInt(userInput.value); //parseInt(value) == +value
+}
+
+function displayEquation(startNumb, symbol, enteredNum) {
+  const calcDescription = `${startNumb} ${symbol} ${enteredNum}`;
+  outputResult(currentResult, calcDescription);
 }
 
 function add() {
   const enteredNum = getUserNumberInput(),
-  calcDescription = `${currentResult}` + ${enteredNum};
-  currentResult += enteredNum; 
+    calcDescription = `${currentResult} + ${enteredNum}`;
+  currentResult += enteredNum;
   outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNum;
+  displayEquation(initialResult, '-', enteredNum);
+}
+
+function multiply() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNum;
+  displayEquation(initialResult, 'x', enteredNum);
+}
+
+function divide() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNum;
+  displayEquation(initialResult, '÷', enteredNum);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
